3be8e13c6f4ec7fe9c7aa5551c9c87911328a927
RDeveLabs <rdevelopamd@gmail.com>
2026-05-15 16:53:41 +0700

    testing endpoint

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.post('/compress', authMiddleware, upload.single('file'), async (req, res) =>
 });
 
 app.get('/data', authMiddleware, (req, res) => {
-  res.json([
+  res.status(200).json([
     {
       id: 1,
       nama: "Raihan Arrasyid Monadika",
